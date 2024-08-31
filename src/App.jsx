36b8cc7143cf8af09ba055b9283d44d217f35d7b
Ekarahma148 <ekaarahma010@gmail.com>
2024-08-31 23:07:03 +0700

    tugas1

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Profil from"./profil.jsx";
+
+export default function App(){
+  return(
+    <section>
+      <h1>Pahlawan Indonesia</h1>
+      <Profil/>
+    </section>
+  );
+}
---
+++ src/profil.jsx
@@ -0,0 +1,9 @@
+export default function Profil(){
+    return(
+        <div className="profil">
+            <img src="https://i.pinimg.com/236x/e1/82/01/e182017acec7cba24c441370920bbe6b.jpg" alt="kosong"/>
+            <img src="https://i.pinimg.com/236x/2e/97/ea/2e97eaae04ac7d44284ad974f50293a2.jpg" alt="kosong"/>
+            <img src="https://i.pinimg.com/236x/85/fe/f0/85fef0b7502a0ec12627dbdaae11ff58.jpg" alt="kosong"/>
+        </div>
+    );
+}
